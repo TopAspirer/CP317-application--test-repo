cece9c16420835a429a0792175fbf835da9a6130
Hans Yang <hans.yangchenghao@gmail.com>
2026-01-17 21:43:46 -0500

    test app

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('F-lite')
+
+col1, col2, col3, col4 = st.columns(4)
+col1.title("a")
+col2.title("a")
+col3.title("a")
+col4.title("a")
